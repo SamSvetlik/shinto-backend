17d2f996cc06b2df280beff72a48883b301ea663
SamSvetlik <sam.svetlik@gmail.com>
2023-06-20 12:50:42 -0500

    chore: connected to database, with secrets hidden. Currently returns all users at /users.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const pool = require('./sql/connection')
 
 const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res)=> {
     res.json({
         message: "Welcome!"
+    })
+})
+
+app.get("/users", (req, res) => {
+    pool.query("SELECT * FROM users", (err, rows, fields) => {
+        res.json(rows)
     })
 })
 
---
+++ sql/users.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    isAdmin TINYINT,
+    memberSince DATE NOT NULL,
+    renewalDate DATE NOT NULL,
+    emergencyContact VARCHAR(255) NOT NULL,
+    emergencyNumber CHAR(10) NOT NULL,
+    age TINYINT NOT NULL,
+    birthday DATE NOT NULL,
+    notes VARCHAR(255),
+    beltRank VARCHAR(255),
+    beltProgress TINYINT,
+    profilePic VARCHAR(255)
+);
+
+INSERT INTO users VALUES (
+    NULL,
+    "Sam Svetlik",
+    "sam.svetlik@gmail.com",
+    "password",
+    0,
+    "2023-06-20",
+    "2023-06-20",
+    "Stephany Perez",
+    "7205797996",
+    32,
+    "1990-08-17",
+    NULL,
+    NULL,
+    NULL,
+    NULL
+)
