b395244cc5ddf7ce4788d8488ccf81422bdff6c0
SamSvetlik <sam.svetlik@gmail.com>
2023-06-21 15:21:08 -0500

    improvement: list and show events now return host name along with id.

--- controllers/events.js
+++ controllers/events.js
@@ -2,7 +2,13 @@ const pool = require('../sql/connection')
 
 const list = (req, res) => {
     // lists all events in the database
-    pool.query("SELECT id, eventName, eventDescription, hostId, DATE_FORMAT(eventTime, '%Y-%m-%d %H:%i:%s') AS formatted_event_time FROM events", (err, rows, fields)=> {
+    const query = `
+        SELECT events.id, events.eventName, events.eventDescription, events.hostId, users.name AS hostName, DATE_FORMAT(events.eventTime, '%Y-%m-%d %H:%i:%s') AS formatted_event_time
+        FROM events
+        JOIN users ON events.hostId = users.id
+    `
+    pool.query(
+        query, (err, rows, fields)=> {
         res.json(rows)
     })
 }
@@ -10,7 +16,13 @@ const list = (req, res) => {
 const show = (req, res) => {
     // shows a single event in the database by id
     const {id} = req.params
-    pool.query(`SELECT id, eventName, eventDescription, hostId, DATE_FORMAT(eventTime, '%Y-%m-%d %H:%i:%s') AS eventTimeString FROM events WHERE id = ${id}`, (err, rows, fields)=> {
+    const query = `
+    SELECT events.id, events.eventName, events.eventDescription, events.hostId, users.name AS hostName, DATE_FORMAT(events.eventTime, '%Y-%m-%d %H:%i:%s') AS formatted_event_time
+    FROM events
+    JOIN users ON events.hostId = users.id
+    WHERE events.id = ${id}
+`
+    pool.query(query, (err, rows, fields)=> {
         res.json(rows)
     })
 }
