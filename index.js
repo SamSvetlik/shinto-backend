01ce3f36eb06d9fea807f38c3d1cc36a5e3febc7
SamSvetlik <sam.svetlik@gmail.com>
2023-06-23 17:18:05 -0500

    chore: added cors to allow server requests from any origin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const pool = require('./sql/connection')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 const signinRoutes = require("./routes/signin")
 const signupRoutes = require("./routes/signup")
 const usersRoutes = require("./routes/users")
@@ -25,7 +26,10 @@ const authenticateToken = (req, res, next) => {
 }
 
 app.use(express.json());
-
+app.use(cors())
+// cors is defaultly configured to allow requests from all origins.
+// before shipping, extra work will need to be done to only allow methods
+// from approved sources, ie the site hosting the front end
 app.use('/signup', signupRoutes)
 app.use('/users',authenticateToken, usersRoutes)
 app.use('/events', eventsRoutes)
